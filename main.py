b5b4de3b725099156b0a9222433bcd75f4923210
Bwori <obriann21@gmail.com>
2019-10-28 10:17:37 +0300

    Adding Enemy

--- main.py
+++ main.py
@@ -1,36 +1,48 @@
 import pygame
+import random
 
 # initialize game
 pygame.init()
 
-#create screen
+# create screen
 screen = pygame.display.set_mode((800, 600))
 
-#Title and Icon
+# Title and Icon
 pygame.display.set_caption("Space Invaders")
-icon = pygame.image.load('logo.png') #32px
+icon = pygame.image.load('logo.png')  # 32px
 pygame.display.set_icon(icon)
 
 # Player
-playerImg = pygame.image.load('space.png') #64px
+playerImg = pygame.image.load('space.png')  # 64px
 playerX = 370
 playerY = 480
 
-# Changing position
+# Changing player position
 playerX_change = 0
+
+# Enemy
+enemyImg = pygame.image.load('enemy.png')  # 64px
+enemyX = random.randint(0, 800)
+enemyY = random.randint(50, 150)
+
+# Changing enemy position
+enemyX_change = 0
 
 
 def player(x, y):
     screen.blit(playerImg, (x, y))
 
 
+def enemy(x, y):
+    screen.blit(enemyImg, (x, y))
 
-#GameLoop
+
+# GameLoop
 running = True
 while running:
 
-    #RGB background
-    screen.fill((64, 224, 208))
+    # RGB background
+    screen.fill((0, 0, 0))
 
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
@@ -46,23 +58,14 @@ while running:
             if event.key == pygame.K_LEFT or event.key == pygame.K_RIGHT:
                 playerX_change = 0
 
-
-
-
     playerX += playerX_change
 
     # the ship does not pass the bounding box
-    if playerX <=0:
+    if playerX <= 0:
         playerX = 0
-    elif playerX >=736:
+    elif playerX >= 736:
         playerX = 736
 
-
-    player(playerX,playerY)
+    player(playerX, playerY)
+    enemy(enemyX, enemyY)
     pygame.display.update()
-
-
-
-
-
-
